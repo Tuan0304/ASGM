915c6ce4ab47ee16e1e0cb770c91fc1e22562ecf
donbmt92 <anctaone0511@gmail.com>
2020-09-27 16:41:43 +0700

    dropdown

---
+++ TranslateApplication/app/src/main/java/com/example/translateapplication/dropdown_menu_dont.java
@@ -0,0 +1,14 @@
+package com.example.translateapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dropdown_menu_dont extends AppCompatActivity {
+//dont
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dropdown_menu);
+    }
+}
