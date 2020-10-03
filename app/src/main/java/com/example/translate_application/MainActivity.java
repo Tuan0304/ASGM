66573343094fe6060c81f512f3ee0d5245a8a16b
donbmt92 <anctaone0511@gmail.com>
2020-10-03 15:46:31 +0700

    xogfragment

--- app/src/main/java/com/example/translate_application/MainActivity.java
+++ app/src/main/java/com/example/translate_application/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.translate_application;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.translate_application.help.HelpFragment;
+import com.example.translate_application.home.HomeFragment;
+import com.example.translate_application.setting.SettingFragment;
+import com.example.translate_application.transoffline.OfflineFragment;
+import com.example.translate_application.voice.VoiceFragment;
+import com.example.translate_application.wordbook.WordBookFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.view.menu.ShowableListMenu;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,7 +35,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     private AppBarConfiguration mAppBarConfiguration;
 
@@ -30,7 +45,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.dropdown_menu);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
 //        FloatingActionButton fab = findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -40,10 +56,9 @@ public class MainActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_wordbook, R.id.nav_voice,R.id.nav_offline,R.id.nav_setting,R.id.nav_help)
                 .setDrawerLayout(drawer)
@@ -51,14 +66,52 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-    }
+//        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new HomeFragment()).commit();
+////        navigationView.bringToFront();
+//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//
+//                switch (item.getItemId()){
+//                    case R.id.nav_home:
+//                  Toast.makeText(MainActivity.this, "alo", Toast.LENGTH_SHORT).show();
+//
+////                        select = new HomeFragment();
+//                        break;
+//                    case R.id.nav_wordbook:
+//
+//
+//                        break;
+//                    case R.id.nav_voice:
+////                        select = new VoiceFragment();
+//                        Toast.makeText(MainActivity.this, "vafaf", Toast.LENGTH_SHORT).show();
+//                        VoiceFragment fragment =  new VoiceFragment();
+//                        fragmentTransaction.add(R.id.nav_voice,fragment);
+//                        fragmentTransaction.commit();
+//                        break;
+//                    case R.id.nav_offline:
+////                        select = new OfflineFragment();
+//                        break;
+//                    case R.id.action_settings:
+////                        select = new SettingFragment();
+//                        break;
+//                    case R.id.nav_help:
+////                        select = new HelpFragment();
+//                        break;
+//                }
+////                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,select).commit();
+//                drawer.closeDrawer(GravityCompat.START);
+//                return true;
+//            }
+//        });
+  }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main2, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main2, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -66,4 +119,11 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_dont, menu);
+//        return true;
+//    }
 }
--- app/src/main/java/com/example/translate_application/voice/VoiceFragment.java
+++ app/src/main/java/com/example/translate_application/voice/VoiceFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -18,6 +19,7 @@ public class VoiceFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_voice, container, false);
+        Toast.makeText(getActivity(), "addd", Toast.LENGTH_SHORT).show();
 
 
         return root;
